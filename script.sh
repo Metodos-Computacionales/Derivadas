a28f1281de7d2e91769b70e950cbd13d173ce78b
alejandra-montoya <j.montoyap@uniandes.edu.co>
2022-08-12 12:05:47 -0500

    Cambios

--- script.sh
+++ script.sh
@@ -1,7 +1,14 @@
+
 function help(){
     echo "debe incluir tres parametros---"
 }
 #Sintaxis del if  
-if [ $# -eq 3]; then 
+#El ! perimete hacer un if no 
+
+if ! [ $# -eq 3]; then 
     echo "Son tres"
-    
+    help
+    exit l
+fi
+# Me enoja que esta cosa no se suba 
+ echo $"Alejandra Montoya "
